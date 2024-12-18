983fb17146d98c8ce86a2801586ac29b210ce89e
krysnand <anandakrishinar@gmail.com>
2024-12-18 08:40:55 -0300

    include class Gestor

---
+++ src/main/java/ifto/projeto/space_management_system/model/Gestor.java
@@ -0,0 +1,30 @@
+package ifto.projeto.space_management_system.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Gestor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "O nome é obrigatório")
+    private String nome;
+
+    @Email(message = "O email deve ser válido")
+    @NotBlank(message = "O email é obrigatório")
+    @Column(unique = true)
+    private String email;
+
+    @NotBlank(message = "O telefone é obrigatório")
+    private String telefone;
+}
